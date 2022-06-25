3eec6672122b60d567c14fcc479c8fb74d66ebf7
Santiago Moncada <santiago.m.a@hotmail.com>
2022-06-26 01:48:58 +0200

    half the size of the assets

--- assets/tileMap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="tileMap" tilewidth="128" tileheight="128" tilecount="18" columns="6">
- <image source="tileMap.png" width="768" height="384"/>
-</tileset>
